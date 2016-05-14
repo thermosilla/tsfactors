390267f3231bdabd60a15e6da986875121edfc37
Tomás Hermosilla <thermosilla@gmail.com>
2016-05-14 20:58:47 -0300

    Repo inicializado
    Package R para la manipulación de dataframes de series de tiempo.
    
    Incluye la función dow, para agregar dias de la semana

---
+++ R/dow.r
@@ -0,0 +1,13 @@
+#' Agrega día de la semana a dataframe a partir de una fecha posix
+#'
+#' Esta función agrega una nueva columna con los días de la semana a un dataframe
+#' @param df Dataframe al que agregar la nueva columna
+#' @param fecha Columna con fechas en formato POSIXt
+#' @keywords dow 
+dow <- function(df, fecha){
+	days = c('lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo')
+	df$DOW <- factor(weekdays.POSIXt(fecha), levels = days)
+	return(df)
+}
+
+
